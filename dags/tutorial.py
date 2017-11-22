000c287af03d2663081c115d5878bc293573448f
Brian Graham <brian@formstack.com>
2017-11-22 14:24:23 -0500

    Add a test dag script

---
+++ dags/tutorial.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 11, 22),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+tutorial_dag = DAG('tutorial', default_args=default_args)
+
+task_one = BashOperator(task_id='print_date', bash_command='date', dag=tutorial_dag)
+task_two = BashOperator(task_id='sleep', bash_command='sleep 5', retries=3, dag=tutorial_dag)
+
+command_template = """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7) }}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
+"""
+
+task_three = BashOperator(
+    task_id='templated_task',
+    bash_command=command_template,
+    params={'my_param': 'Dag, yo'},
+    dag=tutorial_dag
+)
+
+task_two.set_upstream(task_one)
+task_three.set_upstream(task_two)
